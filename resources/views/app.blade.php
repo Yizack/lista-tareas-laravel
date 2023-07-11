9360e6ac663ab3aee66222a01fcd1555781860d6
Yizack Rangel <yizackr@gmail.com>
2023-07-11 11:44:52 -0500

    chore: indent with spaces

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Tareas</title>
-	</head>
-	<body data-bs-theme="light" class="bg-body-tertiary">
-		<div id="app"></div>
-		@vite('resources/js/app.js')
-	</body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tareas</title>
+  </head>
+  <body data-bs-theme="light" class="bg-body-tertiary">
+    <div id="app"></div>
+    @vite('resources/js/app.js')
+  </body>
 </html>
