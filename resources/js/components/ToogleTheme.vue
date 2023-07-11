cec26bd589fd1f678bea20ed1dc34823e995d408
Yizack Rangel <yizackr@gmail.com>
2023-07-11 11:45:27 -0500

    chore: role only for span tag

--- resources/js/components/ToogleTheme.vue
+++ resources/js/components/ToogleTheme.vue
@@ -3,9 +3,9 @@ import IconifyIcon from "./IconifyIcon.vue";
 </script>
 
 <template>
-  <span class="text-dark-emphasis" @click="toogleTheme()">
-    <IconifyIcon v-if="!dark" role="button" icon="ic:outline-wb-sunny" size="18" />
-    <IconifyIcon v-else role="button" icon="ic:outline-nights-stay" size="18" />
+  <span class="text-dark-emphasis" role="button" @click="toogleTheme()">
+    <IconifyIcon v-if="!dark" icon="ic:outline-wb-sunny" size="18" />
+    <IconifyIcon v-else icon="ic:outline-nights-stay" size="18" />
   </span>
 </template>
 
@@ -20,6 +20,10 @@ export default {
     document.body.setAttribute("data-bs-theme", this.dark ? "dark" : "light");
   },
   methods: {
+    /**
+     * Cambiar tema de la aplicación
+     * @method
+     */
     toogleTheme () {
       this.dark = !this.dark;
       localStorage.setItem("dark", this.dark);
