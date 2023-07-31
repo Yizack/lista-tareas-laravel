f249a47be8dc5c420406c93d1e58bc91a6d79c90
Yizack Rangel <yizackr@gmail.com>
2023-07-31 01:11:48 -0500

    test factories and seeders

---
+++ database/factories/TareaFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class TareaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            "nombre" => fake()->sentence(10, true),
+            "completado" => fake()->boolean()
+        ];
+    }
+}
